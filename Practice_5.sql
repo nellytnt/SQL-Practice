90dd6a36c1c9f872d81c737d2a8718a0a63a384a
nellytnt <nellytnt@gmail.com>
2024-10-14 22:36:18 -0400

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -1,1 +1,20 @@
+--EX 1
+select b.Continent, 
+floor(avg(a.Population)) as avg_populations
+from city as a
+join country as b
+on a.CountryCode = b.Code 
+group by b.Continent
+
+--EX 2
+SELECT
+round((sum(case
+      when b.signup_action = 'Confirmed' then 1
+      else 0
+      end) *1.00/ count(signup_action)) ,2) as confirm_rate
+FROM emails as a
+LEFT JOIN texts as b
+ON a.email_id =b.email_id
+
+--EX 3
 
